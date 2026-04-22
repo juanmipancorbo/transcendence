9d365b704b8bd6042b41d421272f2a9f4eb46778
Ziermax <maxi007velazquez@gmail.com>
2026-04-22 20:43:25 +0200

    user-game-model

---
+++ database/initdb.d/initgame.sql
@@ -0,0 +1,13 @@
+CREATE TYPE game_status AS ENUM ('in_progress', 'finished', 'draw', 'aborted');
+
+CREATE TABLE games (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    white_player_id UUID NOT NULL,
+    black_player_id UUID NOT NULL,
+    board_state TEXT DEFAULT NULL,
+    current_turn VARCHAR(10) NOT NULL,
+    game_state game_status DEFAULT 'in_progress',
+    winner_id UUID DEFAULT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
