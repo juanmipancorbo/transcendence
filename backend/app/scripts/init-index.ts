a86c6838aa4f57261bea017bae290a2e17e1648d
Ziermax <maxi007velazquez@gmail.com>
2026-04-28 20:21:38 +0200

    Backend capable of reseting game table

--- backend/app/scripts/init-index.ts
+++ backend/app/scripts/init-index.ts
@@ -1,6 +1,16 @@
 import { init as initUser } from "./user/init";
+import { init as initGame } from "./game/init";
 
-initUser().catch((err) => {
+async function init() {
+  if (process.env.NODE_ENV !== "development") {
+    console.error("Don't even try")
+    process.exit(1);
+  }
+  await initUser();
+  await initGame();
+}
+
+init().catch((err) => {
   console.error("Database Init failed: ", err);
   process.exitCode = 1;
 });
--- backend/app/scripts/seed-index.ts
+++ backend/app/scripts/seed-index.ts
@@ -1,6 +1,14 @@
 import { seed as seedUser } from "./user/seed";
 
-seedUser().catch((err) => {
+async function seed() {
+  if (process.env.NODE_ENV !== "development") {
+    console.error("Don't even try")
+    process.exit(1);
+  }
+  await seedUser();
+}
+
+seed().catch((err) => {
   console.error("Database Seed failed: ", err);
   process.exitCode = 1;
 });
