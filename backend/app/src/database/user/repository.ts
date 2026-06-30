da16d18177cf26393d219eddac37601618350957
cmarrued <carlosmarruedo@hotmail.com>
2026-06-30 15:12:30 +0200

    fix. making it more readable like in register

--- backend/app/src/database/user/repository.ts
+++ backend/app/src/database/user/repository.ts
@@ -2,7 +2,18 @@ import { pool } from "@utils/pg-pool"
 import { sql } from "@utils/sql"
 import { PublicUser, FullUser } from "@endpoints/users-response" 
 
-const PROFILE_DATA = ` id, username, email, avatar_url AS "avatarUrl", current_game AS "currentGame", games_played AS "gamesPlayed", games_won AS "gamesWon", games_lost AS "gamesLost", xp, level`;
+const PROFILE_DATA = `
+  id,
+  username,
+  email,
+  avatar_url AS "avatarUrl",
+  current_game AS "currentGame",
+  games_played AS "gamesPlayed",
+  games_won AS "gamesWon",
+  games_lost AS "gamesLost",
+  xp,
+  level
+`;
 
 // TEST Query DELETE in prod
 export async function selectUserTable(): Promise<FullUser[] | null> {
